3ae9b2464165590cd88f4791a34b51a1b4fc715a
pdimens <pdimens@live.com>
2023-05-22 11:48:42 -0400

    update conda build

--- misc/build.sh
+++ misc/build.sh
@@ -1,38 +1,39 @@
 #! /usr/bin/env bash
 
-export C_INCLUDE_PATH=${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
-
-# create conda PATH, if it doesn't already exist
-mkdir -p $CONDA_PREFIX/bin
-
-# build and install ema
-rm -rf ema|| true
-git clone --recursive https://github.com/EdHarry/ema.git
-cd ema
-git checkout haplotag
-git submodule update --remote
-git apply ../harpy/misc/makefile.patch
-git apply ../harpy/misc/remove_native.patch
-make CC="$CC -fcommon" CXX="$CXX -fcommon" LDFLAGS="$LDFLAGS"
-chmod +x ema
-cp ema $CONDA_PREFIX/bin/ema-h
-cd ..
+mkdir -p PREFIX/bin
+cp misc/ema-h $PREFIX/bin
 
 # Harpy executable
-chmod +x harpy
-cp harpy $CONDA_PREFIX/bin/
+cp harpy $PREFIX/bin/
 
 # rules
-cp rules/*.smk $CONDA_PREFIX/bin/
+cp rules/*.smk $PREFIX/bin/
 
 # associated scripts
 chmod +x utilities/*.{py,R,pl}
-cp utilities/*.{py,R,pl} $CONDA_PREFIX/bin/
+cp utilities/*.{py,R,pl} $PREFIX/bin/
 
 # reports
-chmod +x reports/*.Rmd
-cp reports/*.Rmd $CONDA_PREFIX/bin/
+cp reports/*.Rmd $PREFIX/bin/
+
+
+### deprecated? ###
+#export C_INCLUDE_PATH=${PREFIX}/include
+#export CPLUS_INCLUDE_PATH=${PREFIX}/include
+#export CPP_INCLUDE_PATH=${PREFIX}/include
+#export CXX_INCLUDE_PATH=${PREFIX}/include
+#export LIBRARY_PATH=${PREFIX}/lib
+
+# create conda PATH, if not already existing
+
+# build and install ema
+#rm -rf ema || true
+#git clone --recursive https://github.com/EdHarry/ema.git
+#cd ema && git checkout haplotag
+#git submodule update --remote
+#git apply ../harpy/misc/makefile.patch
+#git apply ../harpy/misc/remove_native.patch
+#make CC="$CC -fcommon" CXX="$CXX -fcommon" LDFLAGS="$LDFLAGS"
+#chmod +x ema
+#cp ema $CONDA_PREFIX/bin/ema-h
+#cd ..
