fd361d5cdd6f14991c9797f10ed125ea1950c6b3
pdimens <pdimens@live.com>
2023-06-06 10:01:01 -0400

    add conda env check

--- misc/buildlocal.sh
+++ misc/buildlocal.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
+if [ -z "$CONDA_PREFIX" ]; then
+    echo "Error: active conda environment not detected."
+    echo "To use this installation script, you need to already be in an active conda environment."
+    exit 1
+fi
+
 mkdir -p ${CONDA_PREFIX}/bin
 cp -n misc/ema-h ${CONDA_PREFIX}/bin
 
