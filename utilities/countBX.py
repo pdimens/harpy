97703dd36a7bae4d3a0da96f37dac8ffd8c9e632
pdimens <pdimens@live.com>
2023-06-09 11:53:57 -0400

    add segment analysis

--- utilities/countBX.py
+++ utilities/countBX.py
@@ -29,14 +29,22 @@ n_valid = 0
 bxz = re.compile('BX:Z:')
 haplotag = re.compile('A[0-9]{2}C[0-9]{2}B[0-9]{2}D[0-9]{2}')
 invalid = re.compile('[AaBbCcDd]00')
-
+inv_dict = {
+    "A" : 0,
+    "B" : 0,
+    "C" : 0,
+    "D" : 0
+}
 with pysam.FastxFile(args.fastqfile) as fh:
     for entry in fh:
         n_reads += 1
         if bxz.match(entry.comment):
             n_bx += 1
             bx = re.search(haplotag, entry.comment).group(0)
-            if invalid.match(bx):
+            inv = re.findall(invalid, bx)
+            if inv:
+                for i in inv:
+                    inv_dict[i[0]] += 1
                 continue
             n_valid += 1
             
@@ -45,3 +53,7 @@ print(f"totalReads\t{n_reads}")
 print(f"bxTagCount\t{n_bx}")
 print(f"bxValid\t{n_valid}")
 print(f"bxInvalid\t{n_bx - n_valid}")
+print("A00\t",str(inv_dict["A"]))
+print("C00\t",str(inv_dict["C"]))
+print("B00\t",str(inv_dict["B"]))
+print("D00\t",str(inv_dict["D"]))
