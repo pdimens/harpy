0da66c35b9e6038e239caaf58021b05878a79e07
pdimens <pdimens@live.com>
2023-06-12 16:49:34 -0400

    add moelcule splitting

--- utilities/bxStats.py
+++ utilities/bxStats.py
@@ -24,14 +24,14 @@ def writestats(x,chr):
         x[bx]["inferred"] = x[bx]["end"] - x[bx]["start"] 
         if x[bx]["mindist"] < 0:
             x[bx]["mindist"] = 0
-        outtext = f"{chr}\t{bx}\t" + "\t".join([str(x[bx][i]) for i in ["n", "start","end", "inferred", "bp", "mindist", "n_exceed"]])
+        outtext = f"{chr}\t{bx}\t" + "\t".join([str(x[bx][i]) for i in ["n", "start","end", "inferred", "bp", "mindist"]])
         #_ = fout.write(outtext + "\n")
         print(outtext, file = sys.stdout)
 
 # write the first line of the output file
 #with open(outfile, "w") as fout:
 #    _ = fout.write("contig\tbx\treads\tstart\tend\tlength_inferred\taligned_bp\tmindist\texceed_50k\n")
-print("contig\tbx\treads\tstart\tend\tlength_inferred\taligned_bp\tmindist\texceed_50k", file = sys.stdout)
+print("contig\tbx\treads\tstart\tend\tlength_inferred\taligned_bp\tmindist", file = sys.stdout)
 
 #alnfile = pysam.AlignmentFile("/home/pdimens/subset.bam")
 
@@ -73,36 +73,57 @@ for read in alnfile.fetch():
             "n":    1,
             "lastpos" : pos_end,
             "mindist" : -1,
-            "n_exceed" : 0
+            "current_suffix": 0
         }
         chromlast = chrm
-    # if BX is present, update
-    else:
-        d[bx]["bp"] += bp
-        d[bx]["n"]  += 1
+        continue
+
+    # if invalid/absent BX, skip the distance stuff
+    if bx in ["noBX", "invalidBX"]:
         chromlast = chrm
-        # if invalid/absent BX, skip the distance stuff
-        if bx in ["noBX", "invalidBX"]:
-            continue
+        continue
 
-        # only if low < currentlow or high > currenthigh
-        if pos_start < d[bx]["start"]:
-            d[bx]["start"] = pos_start
-        if pos_end > d[bx]["end"]:
-            d[bx]["end"] = pos_end
+    orig = bx
+    # section to add current suffix to barcode name, if exists
+    if d[orig]["current_suffix"] > 0:
+        bx = orig + "." + str(d[orig]["current_suffix"])
 
-        # distance from last alignment = current aln start - previous aln end
-        if read.is_forward or (read.is_reverse and not read.is_paired):
-            dist = pos_start - d[bx]["lastpos"]
-            if dist < d[bx]["mindist"] or d[bx]["mindist"] < 0:
-                d[bx]["mindist"] = dist
-            # if the distance between alignments is >50kbp, increment n_exceed.
-            # it's a diagnostic proxy for cases where the same barcode could
-            # be found in alignments that are too far away from each other
-            if dist > 50000:
-                d[bx]["n_exceed"] += 1
+    # distance from last alignment = current aln start - previous aln end
+    if read.is_forward or (read.is_reverse and not read.is_paired):
+        dist = pos_start - d[bx]["lastpos"]
+        # if the distance between alignments is >100kbp, it's a different molecule
+        # so we'll +1 the suffix of the main barcode and relabel this one as BX + suffix
+        # since it's a new entry, we initialize it and move on
+        if dist > 100000:
+            d[orig]["current_suffix"] += 1
+            bx = orig + "." + str(d[orig]["current_suffix"])
+            d[bx] = {
+                "start":  pos_start,
+                "end": pos_end,
+                "bp":   bp,
+                "n":    1,
+                "lastpos" : pos_end,
+                "mindist" : -1,
+                "current_suffix": 0
+            }
+            continue 
+        if dist < d[bx]["mindist"] or d[bx]["mindist"] < 0:
+            d[bx]["mindist"] = dist
 
-        if read.is_reverse or (read.is_forward and not read.is_paired):
-            # set the last position to be the end of current alignment
-            d[bx]["lastpos"] = pos_end
+    # if BX is present, update
+    d[bx]["bp"] += bp
+    d[bx]["n"]  += 1
+    chromlast = chrm
+
+    # only if low < currentlow or high > currenthigh
+    if pos_start < d[bx]["start"]:
+        d[bx]["start"] = pos_start
+    if pos_end > d[bx]["end"]:
+        d[bx]["end"] = pos_end
+
+    if read.is_reverse or (read.is_forward and not read.is_paired):
+        # set the last position to be the end of current alignment
+        d[bx]["lastpos"] = pos_end
+
+
 
