88861005b80537e14fab4a456c1b065b6a292b92
pdimens <pdimens@live.com>
2023-07-07 12:33:12 -0400

    build updates

--- misc/build.sh
+++ misc/build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 mkdir -p ${PREFIX}/bin
-cp misc/ema-h ${PREFIX}/bin
+#cp misc/ema-h ${PREFIX}/bin
 
 # Harpy executable
 cp harpy ${PREFIX}/bin/
@@ -14,25 +14,3 @@ cp utilities/*.{py,R,pl} ${PREFIX}/bin/
 
 # reports
 cp reports/*.Rmd ${PREFIX}/bin/
-
-
-### deprecated? ###
-#export C_INCLUDE_PATH=${PREFIX}/include
-#export CPLUS_INCLUDE_PATH=${PREFIX}/include
-#export CPP_INCLUDE_PATH=${PREFIX}/include
-#export CXX_INCLUDE_PATH=${PREFIX}/include
-#export LIBRARY_PATH=${PREFIX}/lib
-
-# create conda PATH, if not already existing
-
-# build and install ema
-#rm -rf ema || true
-#git clone --recursive https://github.com/EdHarry/ema.git
-#cd ema && git checkout haplotag
-#git submodule update --remote
-#git apply ../harpy/misc/makefile.patch
-#git apply ../harpy/misc/remove_native.patch
-#make CC="$CC -fcommon" CXX="$CXX -fcommon" LDFLAGS="$LDFLAGS"
-#chmod +x ema
-#cp ema $CONDA_PREFIX/bin/ema-h
-#cd ..
