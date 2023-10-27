288c04ad768eab5d083bfef6114e5951f7cc8292
pdimens <pdimens@live.com>
2023-10-27 11:08:43 -0400

    fix logic

--- src/harpy/hpc.py
+++ src/harpy/hpc.py
@@ -10,8 +10,8 @@ except:
     pass
 
 @click.command(no_args_is_help = True)
-@click.option('-o', '--output', required = True, type=click.Path(exists=True), metavar = "Output file", help = 'Name of output file')
-@click.option('-s', '--system', required = True, type = click.Choice(["slurm", "sge"], case_sensitive = False), help = 'HPC scheduling system [slurm, sge]')
+@click.option('-o', '--output', required = True, type=str, metavar = "Output file", help = 'Name of output file')
+@click.option('-s', '--system', required = True, type = click.Choice(["slurm"], case_sensitive = False), help = 'HPC scheduling system [slurm]')
 def hpc(output, system):
     """
     Create a config file to run Harpy on an HPC
@@ -19,6 +19,9 @@ def hpc(output, system):
     With this command you can generate a HPC profile for running
     Harpy on a cluster.
     """
+    if hpc == "sge":
+        click.echo("Unfortunately, SGE is not yet supported.")
+        exit(1)
     if hpc == 'slurm':
         if os.path.exists(output):
             overwrite = input(f"File {output} already exists, overwrite (no|yes)?  ").lower()
