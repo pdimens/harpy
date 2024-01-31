1e0ae5f0c3bf31885f999c50a6eb78a17b3bb0e6
pdimens <pdimens@live.com>
2024-01-31 15:19:22 -0500

    add docstring

--- src/harpy/helperfunctions.py
+++ src/harpy/helperfunctions.py
@@ -14,19 +14,24 @@ import rich_click as click
 
 ## define some rich print functions for less redundancy
 def print_error(errortext):
+    """Print a yellow panel with error text"""
     print(Panel(errortext, title = "[bold]Error", title_align = "left", border_style = "yellow"), file = sys.stderr)
 
 def print_solution(solutiontext):
+    """Print a blue panel with solution text"""
     print(Panel(solutiontext, title = "[bold]Solution", title_align = "left", border_style = "blue"), file = sys.stderr)
 
 def print_solution_with_culprits(solutiontext, culprittext):
+    """Print a blue panel with solution text and the list of offenders below it"""
     print(Panel(solutiontext, title = "[bold]Solution", title_align = "left", subtitle = culprittext, border_style = "blue"), file = sys.stderr)
 
 def print_notice(noticetext):
+    """Print a white panel with information text text"""
     print(Panel(noticetext, title = "Notice", title_align = "left", border_style = "white"), file = sys.stderr)
 
 ## recurring checks and such ##
 def vcfcheck(vcf):
+    """Check that a file ends with one of .vcf, .bcf, or .vcf.gz. Is case insensitive."""
     vfile = vcf.lower()
     if vfile.endswith(".vcf") or vfile.endswith(".bcf") or vfile.endswith(".vcf.gz"):
         pass
