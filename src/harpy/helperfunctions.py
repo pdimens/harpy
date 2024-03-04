c71e064d152b15a66d8ff6186c2c180197afe433
pdimens <pdimens@live.com>
2024-03-04 11:17:02 -0500

    make sure pysam has bounds

--- src/harpy/helperfunctions.py
+++ src/harpy/helperfunctions.py
@@ -73,7 +73,7 @@ def generate_conda_deps():
     """Create the YAML files of the workflow conda dependencies"""
     condachannels = ["conda-forge", "bioconda", "defaults"]
     environ = {
-        "qc" : ["falco", "fastp", "multiqc", "pysam"],
+        "qc" : ["falco", "fastp", "multiqc", "pysam=0.22"],
         "align": ["bwa", "ema","icu","libzlib", "samtools=1.19", "seqtk", "xz"],
         "variants.snp": ["bcftools=1.19", "freebayes=1.3.6"],
         "variants.sv": ["leviathan", "naibr-plus"],
