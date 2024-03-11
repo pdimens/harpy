284800948504e3007a7898ec5451e8d22c8ec4c9
pdimens <pdimens@live.com>
2024-03-11 16:29:25 -0400

    panel width at 75

--- src/harpy/printfunctions.py
+++ src/harpy/printfunctions.py
@@ -6,20 +6,20 @@ from rich.panel import Panel
 def print_onstart(text, title):
     """Print a panel of info on workflow run"""
     print("")
-    print(Panel(text, title = f"[bold]Harpy {title}", title_align = "left", border_style = "light_steel_blue", subtitle= "Running Workflow"), file = sys.stderr)
+    print(Panel(text, title = f"[bold]Harpy {title}", title_align = "left", border_style = "light_steel_blue", subtitle = "Running Workflow", width = 75), file = sys.stderr)
 
 def print_error(errortext):
     """Print a yellow panel with error text"""
-    print(Panel(errortext, title = "[bold]Error", title_align = "left", border_style = "yellow"), file = sys.stderr)
+    print(Panel(errortext, title = "[bold]Error", title_align = "left", border_style = "yellow", width = 75), file = sys.stderr)
 
 def print_solution(solutiontext):
     """Print a blue panel with solution text"""
-    print(Panel(solutiontext, title = "[bold]Solution", title_align = "left", border_style = "blue"), file = sys.stderr)
+    print(Panel(solutiontext, title = "[bold]Solution", title_align = "left", border_style = "blue", width = 75), file = sys.stderr)
 
 def print_solution_with_culprits(solutiontext, culprittext):
     """Print a blue panel with solution text and the list of offenders below it"""
-    print(Panel(solutiontext, title = "[bold]Solution", title_align = "left", subtitle = culprittext, border_style = "blue"), file = sys.stderr)
+    print(Panel(solutiontext, title = "[bold]Solution", title_align = "left", subtitle = culprittext, border_style = "blue", width = 75), file = sys.stderr)
 
 def print_notice(noticetext):
     """Print a white panel with information text text"""
-    print(Panel(noticetext, title = "Notice", title_align = "left", border_style = "dim"), file = sys.stderr)
+    print(Panel(noticetext, title = "Notice", title_align = "left", border_style = "dim", width = 75), file = sys.stderr)
