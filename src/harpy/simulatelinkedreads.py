4cee6c7964809b6a7dfdacf17e685a1f0fb03201
pdimens <pdimens@live.com>
2024-04-06 10:42:31 -0400

    lower min by a lot

--- src/harpy/simulatelinkedreads.py
+++ src/harpy/simulatelinkedreads.py
@@ -11,7 +11,7 @@ import sys
 @click.option('-i', '--distance-sd', type = click.IntRange(min = 1), default = 15, show_default=True,  help = "Standard deviation of read-pair distance")
 @click.option('-b', '--barcodes', type = click.Path(exists=True, dir_okay=False), help = "File of linked-read barcodes")
 @click.option('-n', '--read-pairs', type = click.IntRange(min = 1), default = 600, show_default=True,  help = "Number of read pairs to simulate, in millions")
-@click.option('-l', '--molecule-length', type = click.IntRange(min = 100), default = 100, show_default=True,  help = "Mean molecule length (kbp)")
+@click.option('-l', '--molecule-length', type = click.IntRange(min = 10), default = 100, show_default=True,  help = "Mean molecule length (kbp)")
 @click.option('-p', '--partitions', type = click.IntRange(min = 1), default=1500, show_default=True,  help = "How many partitions to generate (×1000)")
 @click.option('-m', '--molecules-per', type = click.IntRange(min = 1), default = 100, show_default=True,  help = "Average number of molecules per partition")
 @click.option('-t', '--threads', default = 4, show_default = True, type = click.IntRange(min = 1, max_open = True), help = 'Number of threads to use')
