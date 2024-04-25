331b2ff46b30ae22a8e4668120498ec94cdc60f1
pdimens <pdimens@live.com>
2024-04-25 12:56:22 -0400

    move globalscripts to inside the src folder

--- resources/build.sh
+++ resources/build.sh
@@ -3,11 +3,11 @@
 mkdir -p ${PREFIX}/bin
 
 # compilation
-g++ src/harpy/scripts/extractReads.cpp -O3 -o ${PREFIX}/bin/extractReads
+g++ src/harpy/globalscripts/extractReads.cpp -O3 -o ${PREFIX}/bin/extractReads
 
 # install harpy proper
 ${PYTHON} -m pip install . --no-deps -vvv
 
 # associated scripts
-chmod +x globalscripts/* 
-cp globalscripts/* ${PREFIX}/bin/
+chmod +x src/harpy/globalscripts/* 
+cp src/harpy/globalscripts/* ${PREFIX}/bin/
--- resources/buildforCI.sh
+++ resources/buildforCI.sh
@@ -3,9 +3,9 @@
 mkdir -p ${CONDA_PREFIX}/bin
 
 # compilation
-g++ globalscripts/extractReads.cpp -O3 -o ${CONDA_PREFIX}/bin/extractReads
+g++ src/harpy/globalscripts/extractReads.cpp -O3 -o ${CONDA_PREFIX}/bin/extractReads
 
 ## associated scripts
-chmod +x globalscripts/*
-cp globalscripts/* ${CONDA_PREFIX}/bin/
+chmod +x src/harpy/globalscripts/*
+cp src/harpy/globalscripts/* ${CONDA_PREFIX}/bin/
 
