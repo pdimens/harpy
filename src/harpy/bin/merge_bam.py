19db51bc9b8b9826ad0b54f80d35913af338810e
pdimens <pdimens@live.com>
2024-08-01 11:29:26 -0400

    first try

---
+++ src/harpy/bin/merge_bam.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python
+"""Concatenate records from haplotagged BAM files"""
+import os
+import sys
+import argparse
+import pysam
+
+parser = argparse.ArgumentParser(
+    prog = 'merge_bam.py',
+    description =
+    """
+    Concatenate records from haplotagged SAM/BAM files while making sure MI:i tags remain unique for every sample.
+    This is a means of accomplishing the same as \'samtools cat\', except all MI (Molecule Identifier) tags are updated
+    so individuals don't have overlapping MI tags (which would mess up all the linked-read data). You can either provide
+    all the files you want to concatenate, or a single file featuring filenames with the \'-b\' option.
+    """,
+    usage = "merge_bam.py file_1.bam file_2.bam...file_N.bam > output.bam",
+    exit_on_error = False
+    )
+
+parser.add_argument("alignments", nargs='*', help = "SAM or BAM files")
+parser.add_argument("-b", "--bamlist", required = False, type = str, help = "List of SAM or BAM files to concatenate")
+if len(sys.argv) == 1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+args = parser.parse_args()
+
+if (args.alignments and args.bamlist):
+    print("Please provide either a single file to \'--bamlist\' featuring all the files you want to concatenate (one per line) or all the files at the command line.")
+    sys.exit(1)
+
+# global set of observed MI tags
+MI_INVENTORY = set()
+
+if args.bamlist:
+    with open(args.bamlist, "r") as bl:
+        # read in and filter out commented lines
+        aln_list = [i for i in bl.readlines() if not i.startswith("#")]
+else:
+    if isinstance(args.alignments, str):
+        aln_list = [args.alignments]
+    else:
+        aln_list = args.alignments
+
+# process the first bam file, establish a baseline
+first_xam = aln_list.pop(0)
+if first_xam.lower().endswith(".bam"):
+    if not os.path.exists(f"{first_xam}.bai"):
+        pysam.index(first_xam)
+
+xam_in = pysam.AlignmentFile(first_xam)
+bam_out = pysam.AlignmentFile("-", "wb", template = xam_in)
+for record in xam_in.fetch():
+    try:
+        MI_INVENTORY.add(record.get_tag("MI"))
+    except:
+        pass
+    bam_out.write(record)
+xam_in.close()
+
+
+# current available unused MI tag
+MI_NEW = max(MI_INVENTORY) + 1
+
+# iterate through the rest of the bam files
+for xam in aln_list:
+    # create MI dict for this sample
+    MI_LOCAL = {}
+    # create index if it doesn't exist
+    if xam.lower().endswith(".bam"):
+        if not os.path.exists(f"{xam}.bai"):
+            pysam.index(xam)
+    xamfile = pysam.AlignmentFile(xam)
+
+    for record in xamfile.fetch():
+        try:
+            mi = record.get_tag("MI")
+            # if previously converted for this sample, use that
+            if mi in MI_LOCAL:
+                record.set_tag("MI", MI_LOCAL[mi])
+            else:
+                record.set_tag("MI", MI_NEW)
+                # add to sample conversion dict
+                MI_LOCAL[mi] = MI_NEW
+                # add to total manifest of MI tags for all samples
+                MI_INVENTORY.add(MI_NEW)
+                # increment to next unique MI
+                MI_NEW += 1
+        except:
+            pass
+
+        bam_out.write(record)
+    xamfile.close()
+
+bam_out.close()
+
