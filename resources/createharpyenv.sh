6388b8f7bc50f70d28ab3be92a7b48e59b208037
pdimens <pdimens@live.com>
2024-10-09 11:45:17 -0400

    fixes

--- resources/createharpyenv.sh
+++ resources/createharpyenv.sh
@@ -2,11 +2,4 @@
 
 ## Use the first positional argument to set a name, usually `harpy` or `harpytest`
 
-mamba create -n $1 -c bioconda -c conda-forge \
-    bcftools=1.19 \
-    python \
-    pandas \
-    rich-click \
-    snakemake-minimal>7 \
-    samtools \
-    seqtk
+mamba create -n $1 -f harpy.yaml
