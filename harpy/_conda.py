7ed049d2d9d92687779659e5702e2e7b17415c11
pdimens <pdimens@live.com>
2024-10-16 15:32:59 -0400

    update

--- harpy/_conda.py
+++ harpy/_conda.py
@@ -1,4 +1,4 @@
-"""Module that sets up the conda environments and dependencies for all the Harpy workflows"""
+"""Creates environment recipes for all the Harpy workflows"""
 
 import os
 
@@ -16,9 +16,6 @@ def create_conda_recipes():
             "conda-forge::icu",
             "conda-forge::libzlib",
             "conda-forge::xz"
-            ],
-        "spades": [
-            "conda-forge::python=3"
         ],
         "assembly":[
             "bioconda::arcs",
@@ -34,14 +31,14 @@ def create_conda_recipes():
         "phase" : [
             "bioconda::hapcut2",
             "bioconda::whatshap"
-            ],
+        ],
         "qc" : [
             "bioconda::falco",
             "bioconda::fastp",
             "bioconda::multiqc=1.22",
             "bioconda::pysam=0.22",
             "bioconda::quickdeconvolution"
-            ],
+        ],
         "r" : [
             "conda-forge::pandoc",
             "conda-forge::r-dt",
@@ -57,7 +54,7 @@ def create_conda_recipes():
             "conda-forge::r-viridislite", 
             "conda-forge::r-xml2",
             "r::r-biocircos"
-            ],
+        ],
         "simulations" : [
             "alienzj::msort",
             "bioconda::dwgsim=1.1.14",
@@ -66,16 +63,19 @@ def create_conda_recipes():
             "bioconda::perl-parse-recdescent",
             "conda-forge::numpy",
             "conda-forge::perl"
-            ],
+        ],
+        "spades": [
+            "conda-forge::python=3"
+        ],
+        "stitch" : [
+            "bioconda::r-stitch=1.6.10"
+        ],
         "variants": [
             "bioconda::bcftools=1.20",
             "bioconda::freebayes=1.3.6",
             "bioconda::leviathan",
             "bioconda::naibr-plus"
-            ],
-        "stitch" : [
-            "bioconda::r-stitch=1.6.10"
-            ]
+        ]
     }
 
     os.makedirs(".harpy_envs", exist_ok = True)
@@ -89,7 +89,7 @@ def create_conda_recipes():
             yml.write("\n  - ".join(deps) + "\n")
 
     # post-deployment scripts
-    with open(".harpy_envs/assembly.post-deploy.sh", "w", encoding="utf-8") as shellscript:
+    with open(".harpy_envs/spades.post-deploy.sh", "w", encoding="utf-8") as shellscript:
         shellscript.write("wget -O .spades.tar.gz https://github.com/ablab/spades/releases/download/v4.0.0/SPAdes-4.0.0-Linux.tar.gz\n")
         shellscript.write("tar -xvzf .spades.tar.gz && rm .spades.tar.gz\n")
         shellscript.write("mv SPAdes-4.0.0-Linux/bin/* ${CONDA_PREFIX}/bin && mv SPAdes-4.0.0-Linux/share/* ${CONDA_PREFIX}/share\n")
