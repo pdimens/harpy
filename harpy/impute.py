9a4ad269549bb465643378df06976f2e7cef0821
pdimens <pdimens@live.com>
2024-10-18 15:35:10 -0400

    nicer, clearer text

--- harpy/impute.py
+++ harpy/impute.py
@@ -111,7 +111,7 @@ def impute(inputs, output_dir, parameters, threads, vcf, vcf_samples, extra_para
     start_text.add_row("VCF Samples:", f"{len(samplenames)}")
     start_text.add_row("Alignment Files:", f"{n}")
     start_text.add_row("Parameter File:", parameters)
-    start_text.add_row("Usable Contigs:", f"{n_biallelic}")
+    start_text.add_row("Contigs:", f"{n_biallelic} [dim](with at least 2 biallelic SNPs)")
     start_text.add_row("Output Folder:", output_dir + "/")
     start_text.add_row("Workflow Log:", sm_log.replace(f"{output_dir}/", "") + "[dim].gz")
     launch_snakemake(command, "impute", start_text, output_dir, sm_log, quiet, "workflow/impute.summary")
