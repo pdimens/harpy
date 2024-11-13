14dec857a607e3cc23e58f78df9a17cec66247e8
pdimens <pdimens@live.com>
2024-11-13 14:21:51 -0500

    make more efficient following the rabbit's suggestions

--- harpy/bin/inline_to_haplotag.py
+++ harpy/bin/inline_to_haplotag.py
@@ -8,7 +8,7 @@ from itertools import zip_longest, product
 
 parser = argparse.ArgumentParser(
     prog = 'inline_to_haplotag.py',
-    description = 'Converts 10x linked reads to haplotag linked reads with barcodes in BX:Z: and OX:Z: header tags.',
+    description = 'Moves inline linked read barcodes to read headers (OX:Z) and converts them into haplotag ACBD format (BX:Z).',
     usage = "inline_to_haplotag.py -f <forward.fq.gz> -r <reverse.fq.gz> -b <barcodes.txt> -p <prefix> > barcodes.conversion.txt",
     exit_on_error = False
     )
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-f", "--forward", required = True, type = str, help = "Forward reads of paired-end FASTQ file pair (gzipped)")
 parser.add_argument("-r", "--reverse", required = True, type = str, help = "Reverse reads of paired-end FASTQ file pair (gzipped)")
 parser.add_argument("-p", "--prefix", required = True, type = str, help = "Prefix for outfile FASTQ files (e.g. <prefix>.R1.fq.gz)")
-parser.add_argument("-b", "--barcodes", required = True, type=str, help="File listing the 10X barcodes to convert to haplotag format, one barcode per line")
+parser.add_argument("-b", "--barcodes", required = True, type=str, help="File listing the linked-read barcodes to convert to haplotag format, one barcode per line")
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
     sys.exit(1)
@@ -28,14 +28,37 @@ for i in [args.forward, args.reverse, args.barcodes]:
 if err:
     parser.error("Some input files were not found on the system:\n" + ", ".join(err))
 
-def process_record(fw_entry, rv_entry):
+def iter_fastq_records(file_handle):
+    """Iterate over FASTQ records in a file.
+    file_handle: Opened gzip file handle       
+    Yields: FASTQ record [header, seq, '+', qual]
+    Raises ValueError If file is not in FASTQ format
+    """
+    record = []
+    for line in file_handle:
+        line = line.decode().rstrip("\n")
+        record.append(line)
+        if len(record) == 4:
+            if not (record[0].startswith("@") and record[2] == "+"):
+                raise ValueError("Invalid FASTQ format")
+            yield record
+            record = []
+    if record:
+        raise ValueError("Incomplete FASTQ record at end of file")
+
+def validate_barcode(barcode):
+    """Validate barcode format (A,C,G,T)."""
+    if not set(barcode).issubset({'A','C','G','T'}):
+        raise ValueError(f"Invalid barcode format: {barcode}. Barcodes must be captial letters and only contain standard nucleotide values ATCG.")
+
+def process_record(fw_entry, rv_entry, barcode_dict, haplotag_bc):
     """convert the barcode to haplotag"""
     # [0] = header, [1] = seq, [2] = +, [3] = qual
     bc10x = fw_entry[1][:16]
-    bchap = bc_dict.get(bc10x, "A00C00B00D00")
+    bchap = barcode_dict.get(bc10x, "A00C00B00D00")
     if not bchap:
-        bchap = "".join(next(bc_generator))
-        bc_dict[bc10x] = bchap
+        bchap = "".join(next(haplotag_bc))
+        barcode_dict[bc10x] = bchap
     _new_fw  = fw_entry[0].split()[0] + f"\tOX:Z:{bc10x}\tBX:Z:{bchap}\n"
     _new_fw += fw_entry[1][16:] + "\n"
     _new_fw += fw_entry[2] + "\n"
@@ -50,59 +73,30 @@ bc_generator = product("A", bc_range, "C", bc_range, "B", bc_range, "D", bc_rang
 bc_dict = {}
 
 # read in barcodes
-with open(args.barcodes, "r") as bc_file:
+opener = gzip.open if args.barcodes.lower().endswith('.gz') else open
+mode = 'rt' if args.barcodes.lower().endswith('.gz') else 'r'
+with opener(args.barcodes, mode) as bc_file:
     while True:
-        # Get next line from file
-        if args.barcodes.endswith("gz"):
-            line = bc_file.readline().decode()
-        else:
-            line = bc_file.readline()
-        # if line is empty
-        # end of file is reached
+        line = bc_file.readline()
         if not line:
             break
-        bc = line.rstrip("\n").split()
-        _10X = str(bc[0])
-        bc_dict[_10X] = None
+        barcode = line.rstrip().split()[0]
+        validate_barcode(barcode)
+        bc_dict[barcode] = None
 
 # simultaneously iterate the forward and reverse fastq files
-fw_reads = args.forward
-rv_reads = args.reverse
-
 fw_out = gzip.open(f"{args.prefix}.R1.fq.gz", "wb", 6)
 rv_out = gzip.open(f"{args.prefix}.R2.fq.gz", "wb", 6)
 
-with gzip.open(fw_reads, "r") as fw_i, gzip.open(rv_reads, "r") as rv_i:
-    # store the fq records here
-    fw_record = []
-    rv_record = []
-    i = 0
-    for fw, rv in zip_longest(fw_i, rv_i):
-        fw_line = fw.decode().rstrip("\n")
-        rv_line = rv.decode().rstrip("\n")
-        if fw_line.startswith("@") and i > 0:
-            i += 1
-            # process the full record
-            new_fw, new_rv = process_record(fw_record, rv_record)
-            # write new record to files
-            fw_out.write(new_fw.encode("utf-8"))
-            rv_out.write(new_rv.encode("utf-8"))
-            # reset the record
-            fw_record = [fw_line]
-            rv_record = [rv_line]
-        elif fw_line.startswith("@") and i == 0:
-            # its the first record don't write anything
-            i += 1
-            fw_record = [fw_line]
-            rv_record = [rv_line]
-        else:
-            # just append the other lines to the record
-            fw_record.append(fw_line)
-            rv_record.append(rv_line)
+with gzip.open(args.forward, "r") as fw_i, gzip.open(args.reverse, "r") as rv_i:
+    for fw_record, rv_record in zip_longest(iter_fastq_records(fw_i), iter_fastq_records(rv_i)):
+        new_fw, new_rv = process_record(fw_record, rv_record, bc_dict, bc_generator)
+        fw_out.write(new_fw.encode("utf-8"))
+        rv_out.write(new_rv.encode("utf-8"))
 
 fw_out.close()
 rv_out.close()
 
 for i,j in bc_dict.items():
     if j:
-        print(f"{i}\t{j}", file = sys.stdout)
+        sys.stdout.write(f"{i}\t{j}\n")
