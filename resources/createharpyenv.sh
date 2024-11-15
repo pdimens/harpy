4631f81fbec066300068af9f3b3715f085ec5cbe
pdimens <pdimens@live.com>
2024-11-15 11:09:48 -0500

    swap back to conda

--- resources/createharpyenv.sh
+++ resources/createharpyenv.sh
@@ -2,9 +2,9 @@
 
 ## Use the first positional argument to set a name, usually `harpy` or `harpytest`
 
-if command -v mamba &> /dev/null
+if command -v conda &> /dev/null
 then
-    mamba env create -n $1 -f resources/harpy.yaml
+    conda env create -n $1 -f resources/harpy.yaml
 else
     conda env create -n $1 -f resources/harpy.yaml
 fi
