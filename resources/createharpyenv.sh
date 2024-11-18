12be7a08c53abac2a06965d3b0fc39a4c54eefa2
pdimens <pdimens@live.com>
2024-11-18 13:13:44 -0500

    fix logic

--- resources/createharpyenv.sh
+++ resources/createharpyenv.sh
@@ -2,9 +2,9 @@
 
 ## Use the first positional argument to set a name, usually `harpy` or `harpytest`
 
-if command -v conda &> /dev/null
-then
-    conda env create -n $1 -f resources/harpy.yaml
-else
-    conda env create -n $1 -f resources/harpy.yaml
+if ! command -v conda &> /dev/null; then
+    echo "Error: conda is not installed or not in PATH" >&2
+    exit 1
 fi
+
+conda env create -n $1 -f resources/harpy.yaml
