7b3d5ff976f009aaea907652327c8a157070b174
pdimens <pdimens@live.com>
2024-12-06 12:37:57 -0500

    wrap up module

--- harpy/downsample.py
+++ harpy/downsample.py
@@ -3,59 +3,48 @@
 import os
 import re
 import sys
-import yaml
+import pysam
+import random
+import subprocess
 from pathlib import Path
-from rich import box
-from rich.table import Table
 import rich_click as click
-from ._cli_types_generic import SnakemakeParams
-from ._launch import launch_snakemake
-from ._misc import fetch_rule, snakemake_log
+from ._misc import harpy_pulsebar
+
 
 docstring = {
     "harpy downsample": [
         {
             "name": "Parameters",
-            "options": sorted(["--downsample", "--invalid", "--bx-tag", "--random-seed"]),
+            "options": sorted(["--downsample", "--invalid", "--bx-tag", "--random-seed", "--prefix"]),
         },
         {
             "name": "Workflow Controls",
-            "options": ["--output-dir", "--quiet", "--snakemake", "--threads", "--help"],
+            "options": ["--quiet", "--snakemake", "--threads", "--help"],
         },
     ]
 }
 
 @click.command(no_args_is_help = True, context_settings=dict(allow_interspersed_args=False), epilog = "Documentation: https://pdimens.github.io/harpy/workflows/downsample")
-@click.option('-d', '--downsample', type = click.FloatRange(min = 0.000001), help = 'Downsampling amount')
-@click.option('-i', '--invalid', default = "keep", show_default = True, type=click.Choice( ["keep","drop", "downsample"]), help = "Strategy to handle invalid/missing barcodes")
+@click.option('-d', '--downsample', type = click.IntRange(min = 1), help = 'Downsampling amount')
+@click.option('-i', '--invalid', default = "keep", show_default = True, type=click.Choice( ["keep","drop"]), help = "Strategy to handle invalid/missing barcodes")
 @click.option('-b', '--bx-tag', type = str, default = "BX", show_default=True, help = "The header tag with the barcode")
-@click.option('-o', '--output-dir', type = click.Path(exists = False), default = "Downsample", show_default=True,  help = 'Output directory name')
+@click.option('-p', '--prefix', type = click.Path(exists = False), default = "downsampled", show_default = True, help = 'Prefix for output file(s)')
 @click.option('--random-seed', type = click.IntRange(min = 1), help = "Random seed for sampling")
-@click.option('--setup-only',  is_flag = True, hidden = True, show_default = True, default = False, help = 'Setup the workflow and exit')
 @click.option('-t', '--threads', default = 4, show_default = True, type = click.IntRange(min = 1, max_open = True), help = 'Number of threads to use')
 @click.option('--quiet',  is_flag = True, show_default = True, default = False, help = 'Don\'t show output text while running')
-@click.option('--snakemake', type = SnakemakeParams(), help = 'Additional Snakemake parameters, in quotes')
-@click.argument('input', required=True, type=click.Path(exists=True, readable=True), nargs=-1)
-def downsample(input, output_dir, downsample, invalid, bx_tag, random_seed, threads, snakemake, quiet, setup_only):
+@click.argument('input', required=True, type=click.Path(exists=True, readable=True, dir_okay=False), nargs=-1)
+def downsample(input, prefix, downsample, invalid, bx_tag, random_seed, threads, quiet):
     """
     Downsample data by barcode
 
-    Downsamples FASTQ or BAM file(s) by barcode using one of the
-    two methods described below. Input files must be either:
+    Downsamples FASTQ or BAM file(s) by barcode to keep all reads
+    from `-d` barcodes.
     - one BAM file
     - two FASTQ files (R1 and R2 of a paired-end read set)
     
-    ### Downsampling Methods
-    | -d value | unpaired | function |
-    |:---:|:---|:---|
-    | < 1 | dropped | keep `-d` proportion of reads for every barcode |
-    | > 1 | kept | keep all reads from `-d` barcodes |
-
     Use `--invalid` to specify how to handle invalid barcodes:
-    - `keep`: keep all invalid/missing barcodes
+    - `keep`: output all invalid/missing barcodes
     - `drop`: don't output any invalid/missing barcodes
-    - `downsample`: downsample invalid/missing barcodes
-      - (`-d < 1` only)
     """
     # validate input files as either 1 bam or 2 fastq
     if len(bx_tag) != 2:
@@ -75,41 +64,128 @@ def downsample(input, output_dir, downsample, invalid, bx_tag, random_seed, thre
                 badfastq.append(i)
         if badfastq:
             raise click.BadParameter('inputs must be 1 BAM (.bam) file or 2 FASTQ (.fastq|.fq) files. The FASTQ files can be gzipped.')
-    
-    output_dir = output_dir.rstrip("/")
-    workflowdir = os.path.join(output_dir, 'workflow')
-    command = f'snakemake --rerun-incomplete --show-failed-logs --rerun-triggers input mtime params --nolock --cores {threads} --directory . '
-    command += f"--snakefile {workflowdir}/downsample.smk "
-    command += f"--configfile {workflowdir}/config.yaml "
-    if snakemake:
-        command += snakemake
 
-    os.makedirs(workflowdir, exist_ok=True)
-    fetch_rule(workflowdir, "downsample.smk")
-    os.makedirs(f"{output_dir}/logs/snakemake", exist_ok = True)
-    sm_log = snakemake_log(output_dir, "downsample")
-    conda_envs = ["qc"]
-    configs = {
-        "workflow": "downsample",
-        "snakemake_log" : sm_log,
-        "output_directory" : output_dir,
-        "downsample" : downsample if downsample < 1 else int(downsample),       
-        "invalid" : invalid,
-        "bx_tag" :  bx_tag.upper(),
-        **({"random_seed" : random_seed} if random_seed else {}),
-        "workflow_call" : command.rstrip(),
-        "inputs": [Path(i).as_posix() for i in input]
-        }
-    with open(os.path.join(workflowdir, 'config.yaml'), "w", encoding="utf-8") as config:
-        yaml.dump(configs, config, default_flow_style= False, sort_keys=False, width=float('inf'))
+    output_bam = f"{prefix}.bam"
+    bx_tag = bx_tag.upper()
+    rng = random.Random(random_seed).sample if random_seed else random.Random().sample
+    logfile = open(f"{prefix}.log", "w")
+    sys.stderr.write = logfile.write
 
-    if setup_only:
-        sys.exit(0)
+    with harpy_pulsebar(quiet, f"sorting by {bx_tag} tags") as progress:
+        progress.add_task(f"sorting by {bx_tag} tags", total = None)
+        if len(input) == 2:
+            samtools_import = subprocess.Popen(f"samtools import -@ {threads} -T * {input[0]} {input[1]}".split(), stdout=subprocess.PIPE, stderr=logfile) 
+            subprocess.run(f"samtools sort -@ {threads} -o bx_sorted.bam -t {bx_tag}".split(), stdin = samtools_import.stdout, stderr=logfile)
+        else:
+            subprocess.run(f"samtools sort -@ {threads} -o bx_sorted.bam -t {bx_tag} {input[0]}".split(), stderr=logfile)
+    sorted_bam = Path("bx_sorted.bam").resolve().as_posix()
+    invalid = re.compile(r'[AaBbCcDd]00')
 
-    start_text = Table(show_header=False,pad_edge=False, show_edge=False, padding = (0,0), box=box.SIMPLE)
-    start_text.add_column("detail", justify="left", style="light_steel_blue", no_wrap=True)
-    start_text.add_column("value", justify="left")
-    start_text.add_row("Downsample:", f"within {bx_tag} tags" if downsample < 1 else f"across {bx_tag} tags")
-    start_text.add_row("Output Folder:", output_dir + "/")
-    start_text.add_row("Workflow Log:", sm_log.replace(f"{output_dir}/", "") + "[dim].gz")
-    launch_snakemake(command, "deconvolve", start_text, output_dir, sm_log, quiet, "workflow/deconvolve.summary")
+    # read input file, get list of valid barcodes, subsample valid barcodes, read input again, output only sampled barcodes
+    with (
+        pysam.AlignmentFile(sorted_bam, "rb", check_sq=False) as infile,
+        harpy_pulsebar(quiet, f"parsing {bx_tag} tags") as progress
+    ):
+        progress.add_task(f"parsing {bx_tag} tags", total = None)
+        barcodes = set()
+        for record in infile:
+            try:
+                barcode = record.get_tag(bx_tag)
+                if isinstance(barcode, int):
+                    pass # an int from an MI-tharype tag
+                elif invalid.search(barcode):
+                    continue
+            except KeyError:
+                continue
+            barcodes.add(barcode)
+        n_bc = len(barcodes)
+        if downsample > n_bc:
+            sys.stderr.write(f"The number of intended barcodes to downsample to ({downsample}) is greater than the number of barcodes in the input file ({n_bc}). Please choose a smaller number to downsample to.\n")
+            sys.exit(1)
+        barcodes = rng(sorted(barcodes), downsample)
+    with (
+        pysam.AlignmentFile(sorted_bam, "rb", check_sq=False) as infile,
+        pysam.AlignmentFile(output_bam, "wb", template=infile) as outfile,
+        harpy_pulsebar(quiet, f"downsampling {bx_tag} tags") as progress
+    ):
+        progress.add_task(f"downsampling {bx_tag} tags", total = None)
+        for record in infile:
+            try:
+                barcode = record.get_tag(bx_tag)
+                if isinstance(barcode, int):
+                    pass # an int from an MI-type tag
+                elif invalid.search(barcode):
+                    if invalid == "keep":
+                        outfile.write(record)
+                    continue
+            except KeyError:
+                if invalid == "keep":
+                    outfile.write(record)
+                continue
+            if barcode in barcodes:
+                outfile.write(record)
+
+    # convert back
+    os.remove(sorted_bam)
+    if len(input) == 2:
+        subprocess.run(f'samtools fastq -T * -c 6 -1 {prefix}.R1.fq.gz -2 {prefix}.R2.fq.gz {output_bam}'.split(), stderr=logfile)
+        os.remove(output_bam)
+    logfile.close()
+
+
+
+# store records with the same barcode in an array
+# then subsample according to the downsample fraction and write to output
+# "within":
+#        with (
+#            pysam.AlignmentFile(sorted_bam, "rb", check_sq=False) as infile,
+#            pysam.AlignmentFile(output_bam, "wb", template=infile) as outfile,
+#            harpy_pulsebar(quiet, f"downsampling within {bx_tag} tags") as progress
+#        ):
+#            progress.add_task(f"downsampling within {bx_tag} tags", total = None)
+#            record_store_F = []
+#            record_store_R = []
+#            last_barcode = None
+#            for record in infile:
+#                try:
+#                    barcode = record.get_tag(bx_tag)
+#                    if isinstance(barcode, int):
+#                        pass
+#                    elif invalid.search(barcode):
+#                        if invalid == "keep":
+#                            outfile.write(record)
+#                        elif invalid == "downsample":
+#                            if rng.uniform(0, 1) <= downsample:
+#                                outfile.write(record)    
+#                        continue
+#                except KeyError:
+#                    if invalid == "keep":
+#                        outfile.write(record)
+#                    elif invalid == "downsample":
+#                        if rng.uniform(0, 1) <= downsample:
+#                            outfile.write(record)    
+#                    continue
+#                    
+#                if last_barcode and barcode != last_barcode:
+#                    # subsample records to file and reset record stores
+#                    for i,j in zip(record_store_F, record_store_R):
+#                        if rng.uniform(0, 1) <= downsample:
+#                            if i:
+#                                outfile.write(i)
+#                            if j:
+#                                outfile.write(j)
+#                    record_store_F = []
+#                    record_store_R = []
+#                # otherwise proceed to append record to stores
+#                if record.is_forward and record.is_paired:
+#                    record_store_F.append(record)
+#                elif record.is_reverse and record.is_paired:
+#                    record_store_R.append(record)    
+#                elif record.is_forward and not record.is_paired:
+#                    record_store_F.append(record)
+#                    record_store_R.append(None)
+#                elif record.is_reverse and not record.is_paired:
+#                    record_store_F.append(None)
+#                    record_store_R.append(record)
+#                # update what the last barcode is
+#                last_barcode = barcode
