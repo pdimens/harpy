3106a447d86cddd43d577b553b833966fa4db824
pdimens <pdimens@gmail.com>
2025-01-28 16:57:36 -0500

    add this for dev reasons

---
+++ resources/rebuild_pixi.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+if [ -z "$CONDA_PREFIX" ]; then
+    echo "Error: active conda environment not detected."
+    echo "To use this installation script, you need to already be in an active conda environment."
+    exit 1
+fi
+
+mkdir -p ${CONDA_PREFIX}/bin
+
+# compilation
+g++ harpy/bin/extractReads.cpp -O3 -o ${CONDA_PREFIX}/bin/extractReads
+
+# associated scripts
+chmod +x harpy/bin/* 
+cp -f harpy/bin/* ${CONDA_PREFIX}/bin/
