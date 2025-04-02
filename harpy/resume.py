3438bdc6205d4ab01beb527205f13bc3d89cbbd7
pdimens <pdimens@gmail.com>
2025-04-02 08:56:53 -0400

    update to new profile system, some fixes

--- harpy/resume.py
+++ harpy/resume.py
@@ -9,7 +9,7 @@ from ._conda import check_environments
 from ._cli_types_generic import convert_to_int
 from ._printing import print_error, workflow_info
 from ._launch import launch_snakemake
-from ._misc import snakemake_log
+from ._misc import snakemake_log, write_workflow_config
 from ._conda import create_conda_recipes
 
 @click.command(context_settings=dict(allow_interspersed_args=False), epilog = "Documentation: https://pdimens.github.io/harpy/workflows/other")
@@ -33,9 +33,13 @@ def resume(directory, conda, threads, quiet):
     """
     directory = directory.rstrip("/")
     if not os.path.exists(f"{directory}/workflow/config.yaml"):
-        print_error("missing config file", f"Target directory [blue]{directory}[/blue] does not contain the file [bold]workflow/config.yaml[/bold]")
+        print_error("missing snakemake config", f"Target directory [blue]{directory}[/blue] does not contain the file [bold]workflow/config.yaml[/bold]")
         sys.exit(1)
-    with open(f"{directory}/workflow/config.yaml", 'r', encoding="utf-8") as f:
+    if not os.path.exists(f"{directory}/workflow/config.harpy.yaml"):
+        print_error("missing workflow config", f"Target directory [blue]{directory}[/blue] does not contain the file [bold]workflow/config.harpy.yaml[/bold]")
+        sys.exit(1)
+    
+    with open(f"{directory}/workflow/config.harpy.yaml", 'r', encoding="utf-8") as f:
         harpy_config = yaml.full_load(f)
     conda_envs = harpy_config["conda_environments"] 
     if conda:
@@ -45,9 +49,14 @@ def resume(directory, conda, threads, quiet):
     
     workflow = harpy_config["workflow"].replace(" ", "_")
     sm_log = snakemake_log(directory, workflow)
-    command = harpy_config["workflow_call"] + f" --config snakemake_log={sm_log}"
+    if sm_log != harpy_config["snakemake_log"]:
+        harpy_config["snakemake_log"] = sm_log
+        ## overwrite config file with this updated one, where only the snakemake log has been changed ##
+        write_workflow_config(harpy_config, directory)
+
+    command = harpy_config["snakemake_command"]
     if threads:
-        command = re.sub(r"--threads \d+", f"--threads {threads}", command)
+        command = re.sub(r"--cores \d+", f"--cores {threads}", command)
     start_text = workflow_info(
         ("Output Folder:", directory + "/"),
         ("Workflow Log:", sm_log.replace(f"{directory}/", "") + "[dim].gz")
