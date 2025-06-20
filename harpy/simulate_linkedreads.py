7ff8aac2946906188bc6bbfe9a736af7eb17a412
pdimens <pdimens@gmail.com>
2025-06-20 13:55:57 -0400

    fix docstring

--- harpy/simulate_linkedreads.py
+++ harpy/simulate_linkedreads.py
@@ -16,17 +16,17 @@ docstring = {
     "harpy simulate linkedreads": [
         {
             "name": "Read Simulation Parameters",
-            "options": ["--circular", "--coverage","--distance","--error", "--length",  "--regions", "--stdev"],
+            "options": ["--coverage","--distance","--error", "--length",  "--regions", "--stdev"],
             "panel_styles": {"border_style": "dim blue"}
         },
         {
             "name": "Linked Read Parameters",
-            "options": ["--lr-type", "--molecule-attempts", "--molecule-coverage", "--molecule-length", "--molecules-per"],
+            "options": ["--circular", "--lr-type", "--molecule-attempts", "--molecule-coverage", "--molecule-length", "--molecules-per", "--singletons"],
             "panel_styles": {"border_style": "dim magenta"}
         },
         {
             "name": "Workflow Options",
-            "options": ["--container", "--help", "--hpc", "--output-prefix", "--output-type", "--quiet", "--snakemake", "--threads", "--version"],
+            "options": ["--container", "--help", "--hpc", "--output-prefix", "--output-type", "--quiet", "--seed", "--snakemake", "--threads", "--version"],
             "panel_styles": {"border_style": "dim"}
         }
     ]
@@ -34,15 +34,15 @@ docstring = {
 
 @click.command(epilog = "Documentation: https://pdimens.github.io/mimick/", no_args_is_help = True)
 #Paired-end FASTQ simulation using pywgsim
-@click.option('--coverage', help='mean coverage target for simulated data', show_default=True, default=30, type=click.FloatRange(min=0.05))
-@click.option('--distance', help='outer distance between the two ends in bp', default=500, show_default=True, type=click.IntRange(min=0))
+@click.option('--coverage', help='mean coverage (depth) target for simulated data', show_default=True, default=30, type=click.FloatRange(min=0.05))
+@click.option('--distance', help='outer distance between the two read ends in bp', default=500, show_default=True, type=click.IntRange(min=0))
 @click.option('--error', help='base error rate', default=0.02, show_default=True, type=click.FloatRange(min=0, max=1))
 @click.option('--extindels', help='indels extension rate', default=0, hidden=True, type=click.FloatRange(min=0, max=1))
 @click.option('--indels', help='indels rate', default=0, hidden=True, type=click.FloatRange(min=0, max=1))
 @click.option('--length', help='length of reads in bp', default=150, show_default=True, type=click.IntRange(min=30))
 @click.option('--mutation', help='mutation rate', default=0, hidden=True, type=click.FloatRange(min=0))
 @click.option('--regions', help='one or more regions to simulate, in BED format', type = click.Path(dir_okay=False, readable=True, resolve_path=True))
-@click.option('--stdev', help='standard deviation of --distance', default=50, show_default=True, type=click.IntRange(min=0))
+@click.option('--stdev', help='standard deviation for `--distance`', default=50, show_default=True, type=click.IntRange(min=0))
 #Linked-read simulation
 @click.option('-C','--circular', is_flag= True, default = False, help = 'contigs are circular/prokaryotic')
 @click.option('-l', '--lr-type', help='type of linked-read experiment', default = "haplotagging", show_default=True, show_choices=True, type= click.Choice(["10x", "stlfr", "haplotagging", "tellseq"], case_sensitive=False))
@@ -50,10 +50,10 @@ docstring = {
 @click.option('-c','--molecule-coverage', help='mean percent coverage per molecule if <1, else mean number of reads per molecule', default=0.2, show_default=True, type=click.FloatRange(min=0.00001))
 @click.option('-m','--molecule-length', help='mean length of molecules in bp', show_default=True, default=80000, type=click.IntRange(min=50))
 @click.option('-n','--molecules-per', help='mean number of unrelated molecules per barcode, where a negative number (e.g. `-2`) will use a fixed number of unrelated molecules and a positive one will draw from a Normal distribution', default=2, show_default=True, type=int)
-@click.option('-s','--singletons', help='proportion of barcodes will only have a single read pair', default=0, show_default=True, type=click.FloatRange(0,1))
+@click.option('-s','--singletons', help='proportion of barcodes that will only have one read pair', default=0, show_default=True, type=click.FloatRange(0,1))
 # general
 @click.option('-o','--output-prefix', help='output file prefix', type = click.Path(exists = False, writable=True, resolve_path=True), default = "Simulate/linkedreads/SIM", show_default=True)
-@click.option('-O','--output-type', help='output format of FASTQ files', type = click.Choice(["10x", "stlfr", "standard", "haplotagging", "tellseq"], case_sensitive=False))
+@click.option('-O','--output-type', help='FASTQ output format', type = click.Choice(["10x", "stlfr", "standard", "haplotagging", "tellseq"], case_sensitive=False))
 @click.option('-S','--seed', help='random seed for simulation', type=click.IntRange(min=1), default=None)
 @click.option('-t','--threads', help='number of threads to use for simulation', type = click.IntRange(1, 999, clamp = True), default=2, show_default=True)
 @click.option('--hpc',  type = HPCProfile(), help = 'HPC submission YAML configuration file')
@@ -67,29 +67,20 @@ def linkedreads(barcodes, fasta, output_prefix, output_type, regions, threads,co
     """
     Create linked reads using genome haplotypes
 
-    Barcodes can be supplied one of two ways:
+    This workflow is a very thin veneer to use `Mimick` with mutations (SNPs and indels) deactivated.
+    For all the features, you are encouraged to install `Mimick` from Bioconda and use it directly. You may specify the
+    linked-read barcode chemistry to simulate via `--lr-type` as well as the `--output-type` of FASTQ files (default: same as `lr-type`). See the
+    [Mimick documentation](https://pdimens.github.io/mimick/#/data_formats) for a thorough explanation of the chemistries
+    and output formats. Input barcodes can be supplied one of two ways:
    
     1. randomly generate barcodes based on a specification of `length,count`
         - two integers, comma-separated, no space
         - e.g. `16,400000` would generate 400,000 unique 16bp barcodes 
     2. providing a file of nucleotide barcodes, 1 per line
-
-    You can specify the linked-read barcode chemistry to simulate via `--lr-type` as well as
-    the output format of FASTQ files (default: same as `lr-type`).
-
-    | --lr-type | Format |
-    |:------------------|:-------|
-    |`10x`/`tellseq`   | single barcode on R1 |
-    |`haplotagging`  | R1 and R2 each have different combinatorial 2-barcodes |
-    |`stlfr`         | combinatorial 3-barcode on R2 |
-
-    | --output-type | Barcode Location | Example |
-    |:-----------------|:-------|:---------------------|
-    |`10x`           | start of R1 sequence | `ATAGACCATAGA`GGACA... |
-    |`haplotagging`  | sequence header as `BX:Z:ACBD` |  `@SEQID BX:Z:A0C331B34D87` |
-    |`standard`      | sequence header as `BX:Z:BARCODE`, no specific format | `@SEQID BX:Z:ATACGAGACA` |
-    |`stlfr`         | appended to sequence ID via `#1_2_3` | `@SEQID#1_354_39` |
-    |`tellseq`       | appended to sequence ID via `:ATCG` | `@SEQID:TATTAGCAC` |
+        ```
+        ATGCAGGA
+        GGAGGACT
+        ```
     """
     workflow = "simulate_linkedreads"
     output_dir = os.path.dirname(output_prefix)
