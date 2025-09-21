66be999cd0ef866a9fe677091b1f5466d4fe162e
pdimens <pdimens@gmail.com>
2025-09-21 13:20:20 -0400

    housekeeping

--- harpy/commands/convert.py
+++ harpy/commands/convert.py
@@ -6,7 +6,7 @@ import rich_click as click
 @click.group(no_args_is_help = True, deprecated=True)
 def convert():
     """
-    Convert between linked-read formats and barcode styles
+    Convert between linked-read formats
 
     This module of Harpy has been deprecated and its function has been moved to the Djinn package,
     which should be provided with the standard conda-based Harpy installation.
