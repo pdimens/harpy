07d8251d5267f7b80396c477d45565a0da68f5b4
pdimens <pdimens@gmail.com>
2026-01-01 13:23:54 -0500

    figured out altair defaults!

--- harpy/report/components.py
+++ harpy/report/components.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from IPython.display import display, HTML
 import itables
 
@@ -21,18 +22,20 @@ class colored_boxes:
   letter-spacing: 1px;">{}</div><div style="font-size: 43px; font-weight: bold;">{}</div></div>'''
     _html = _html.format(color, width, height, label, _val)
     self.boxes.append(_html)
+    return self
 
   def render(self, gap = 15):
     '''Display all the colored boxes stored in `self.boxes` in one continuous wrapped row'''
     html_content = '<div style="display: flex; flex-wrap: wrap; gap: {}px; width: 100%;">{}</div>'
     return display(HTML(html_content.format(gap, "\n".join(self.boxes))))
 
-def display_multitext(*args):
-    '''HTML-print all arguments with line breaks between them'''
-    _html = "<p>{}<p>".format("<br>".join(str(i) for i in [*args]))
+def print_time(*args):
+    '''HTML-print the time and all arguments with line breaks between them'''
+    _now = datetime.now().strftime("🗓️ %d %B, %Y 🕔 %H:%M")
+    _html = "<p>{}<p>".format("<br>".join([_now] + [str(i) for i in [*args]]))
     return display(HTML(_html))
 
-def standard_itable(data, filename:str, caption: str|None= None, fixedcols: int|None = None, coldefs = []):
+def standard_itable(data, filename:str, caption: str|None= None, fixedcols: int|None = None, coldefs = [], html: bool = False):
     '''
     Boilerplate version of `itables.show` to reduce in-report boilerplate. Use `filename` to specify the output prefix for
     exports, `caption` to set an optional caption, `fixcols` to freeze this-many columns on the left, and `coldefs` to
@@ -40,17 +43,39 @@ def standard_itable(data, filename:str, caption: str|None= None, fixedcols: int|
     '''
     return itables.show(
         data,
-        caption=caption,
-        buttons=[
+        caption = caption,
+        buttons = [
             "pageLength", "colvis",
             {"extend": "csvHtml5", "title": filename},
             {"extend": "excelHtml5", "title": filename}
         ],
         fixedColumns = {"start": fixedcols} if fixedcols else {},
-        ordering={"indicators": False, "handler": False},
-        columnControl=[["order", "searchDropdown"]],
-        showIndex=False,
-        scrollX = True,
-        columnDefs=coldefs
+        ordering = {"indicators": False, "handler": False},
+        columnControl = [["order", "searchDropdown"]],
+        showIndex= False,
+        scrollX =  True,
+        classes = "display nowrap compact",
+        allow_html=html,
+        columnDefs = coldefs
     )
-    
+
+def proportionbar(value, max_value):
+    """
+    Create an HTML bar for a given value. This is intended to create a
+    proportion-bar formatting for values in a pandas column by way of
+    ```
+    max_val = df['ValidBarcodes'].max()
+
+    df_styled['ValidBarcodes'] = df['ValidBarcodes'].apply(lambda x: proportionbar(x, max_val))
+    ```
+    """
+    percentage = (value / max_value) * 100 if max_value > 0 else 0
+    return f'''
+        <div style="width: 100%; display: flex; align-items: center;">
+            <div style="flex-grow: 1; background: linear-gradient(90deg, 
+                #5cb85c {percentage}%, transparent {percentage}%); 
+                padding: 2px 5px;">
+                {value}
+            </div>
+        </div>
+    '''
